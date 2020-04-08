77365a79fa2d3be1e5d23355d5b0fe99f9678a9d
hh <hh@hhmbp15.local>
2020-04-09 00:09:57 +0100

    implemented Login component and PrivateRoute component

---
+++ friends/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+// setting up React Router
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+// import Login
+import Login from './components/Login';
+import FriendsList from './components/FriendsList';
+import PrivateRoute from './components/PrivateRoute';
+
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/protected">Protected Page</Link>
+          </li>
+        </ul>
+        {/* <Route component={Login} />
+        <Route path="/login" component={Login} />
+        <PrivateRoute exact path="/protected" component={FriendsList} /> */}
+
+
+        <Switch>
+          <PrivateRoute exact path="/protected" component={FriendsList} />
+          <Route path="/login" component={Login} />
+          <Route component={Login} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FriendsList = props => {
+    return (
+        <div>
+            <h1>Friends List</h1>
+        </div>
+    )
+}
+
+export default FriendsList;
