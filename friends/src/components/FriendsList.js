a527f256b2ee5b647bedffa078c1bebffca8c933
hh <hh@hhmbp15.local>
2020-04-09 01:17:00 +0100

    implemented FriendsList component which renders list of friends data from api but without a form to add new friends

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,11 +1,54 @@
 import React from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
-const FriendsList = props => {
-    return (
-        <div>
-            <h1>Friends List</h1>
-        </div>
-    )
+// const FriendsList = props => {
+//     return (
+//         <div>
+//             <h1>Friends List</h1>
+//         </div>
+//     )
+// }
+
+class FriendsList extends React.Component {
+    state = {
+        friends: []
+    };
+
+    componentDidMount() {
+        this.getData();
+    }
+
+    getData = () => {
+        axiosWithAuth()
+            .get("/api/friends")
+            .then(res => {
+                console.log(res)
+                this.setState({
+                    friends: res.data
+                })
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+    render() {
+        console.log(this.state)
+        return (
+            <div>
+                <h1>Friends List:</h1>
+                <div>
+                    {this.state.friends.map(friend => (
+                        <div key={friend.id}>
+                            <h2>{friend.name}</h2>
+                            <p>Age: {friend.age}</p>
+                            <p>Email: {friend.email}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        )
+    }
 }
 
 export default FriendsList;
